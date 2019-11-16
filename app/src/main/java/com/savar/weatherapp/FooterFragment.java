32defb27612265b86a99973221fa1b7596969ea8
mehdi <mehdi.behjati048@gmail.com>
2019-11-16 16:48:06 +0330

    Chenge2_Ui_Fragment

---
+++ app/src/main/java/com/savar/weatherapp/FooterFragment.java
@@ -0,0 +1,37 @@
+package com.savar.weatherapp;
+
+
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class FooterFragment extends Fragment {
+
+
+    public FooterFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_footer, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+    }
+}
