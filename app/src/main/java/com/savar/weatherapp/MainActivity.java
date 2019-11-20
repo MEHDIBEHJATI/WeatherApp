5886015c626a787f0215cfefb091dfdceb46de43
mehdi <mehdi.behjati048@gmail.com>
2019-11-20 23:22:26 +0330

    chenge10_Derawer

--- app/src/main/java/com/savar/weatherapp/MainActivity.java
+++ app/src/main/java/com/savar/weatherapp/MainActivity.java
@@ -3,6 +3,8 @@ package com.savar.weatherapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,18 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+
     }
+//        MENU_START
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.main_menu, menu);
+
+        return super.onCreateOptionsMenu(menu);
+    }
+//    MENU_END
 }
