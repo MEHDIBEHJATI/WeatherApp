38f826468d9cf7ee43df93dc697fbb608406ce60
mehdi <mehdi.behjati048@gmail.com>
2019-11-29 17:15:42 +0330

    chenge25_HttpURLConnection

--- app/src/main/java/com/savar/weatherapp/MainActivity.java
+++ app/src/main/java/com/savar/weatherapp/MainActivity.java
@@ -9,6 +9,11 @@ import android.util.Log;
 import android.view.Menu;
 import android.view.MenuInflater;
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
@@ -30,17 +35,45 @@ public class MainActivity extends AppCompatActivity {
 
 
 
-//        Thread_Start
+//        Thread_Http_Start
 
-        Thread thread = new Thread(new Runnable() {
+        new Thread(new Runnable() {
             @Override
             public void run() {
 
-            }
-        });
-        thread.start();
+                try {
+                    URL obj = new URL("https://google.com");
 
-//        Thread_End
+                    HttpURLConnection con = (HttpURLConnection) obj.openConnection();
+                    con.setRequestMethod("GET");
+                    con.setRequestProperty("User-Agent", "Mozilla/5.0");
+
+                    int responseCode = con.getResponseCode();
+
+                    if (responseCode == HttpURLConnection.HTTP_OK) {
+
+                        BufferedReader in = new BufferedReader(new InputStreamReader(
+                                con.getInputStream()));
+
+                        String inputLine;
+                        StringBuffer response = new StringBuffer();
+                        while ((inputLine = in.readLine()) != null) {
+                            response.append(inputLine);
+                        }
+
+
+                        System.out.println(response.toString());
+                    }
+                }catch (Exception e){
+                    e.printStackTrace();
+                }
+
+
+            }
+        }).start();
+
+
+//        Thread_Http_End
 
     }
 //        MENU_START
