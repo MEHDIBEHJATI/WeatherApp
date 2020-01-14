0439e6755003d5ff83f834b9ceb959e793c7d344
mehdi <mehdi.behjati048@gmail.com>
2020-01-15 01:16:09 +0330

    Change 100_Toggle

--- app/src/main/java/com/savar/weatherapp/MainActivity.java
+++ app/src/main/java/com/savar/weatherapp/MainActivity.java
@@ -1,6 +1,8 @@
 package com.savar.weatherapp;
 
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.drawerlayout.widget.DrawerLayout;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
@@ -8,6 +10,10 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuInflater;
+import android.widget.TextView;
+import androidx.appcompat.widget.Toolbar;
+
+import org.json.JSONObject;
 
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
@@ -16,12 +22,26 @@ import java.net.URL;
 
 public class MainActivity extends AppCompatActivity {
 
+
+   public DrawerLayout drawer ;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
 
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+
+        drawer =  findViewById(R.id.drawer_layout);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
+                this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawer.addDrawerListener(toggle);
+        toggle.syncState();
+
+
+        final TextView txtInt = findViewById(R.id.txtInt);
 
 
 //        Recycler_Start
@@ -30,19 +50,20 @@ public class MainActivity extends AppCompatActivity {
         RecyclerAdapter adapter = new RecyclerAdapter();
         recycler.setAdapter(adapter);
 
-        recycler.setLayoutManager(new LinearLayoutManager(MainActivity.this,RecyclerView.HORIZONTAL,false));
+        recycler.setLayoutManager(new LinearLayoutManager(MainActivity.this, RecyclerView.HORIZONTAL, false));
 //        Recycler_End
 
 
-
-//        Thread_Http_Start
+//        Thread_Start
 
         new Thread(new Runnable() {
             @Override
             public void run() {
 
+
+//        Http_Start
                 try {
-                    URL obj = new URL("https://google.com");
+                    URL obj = new URL("https://weather-ydn-yql.media.yahoo.com/forecastrss?location=sunnyvale,ca&format=json");
 
                     HttpURLConnection con = (HttpURLConnection) obj.openConnection();
                     con.setRequestMethod("GET");
@@ -63,19 +84,31 @@ public class MainActivity extends AppCompatActivity {
 
 
                         System.out.println(response.toString());
+
+                        JSONObject object = new JSONObject(response.toString());
+                        String result = object.getString("location");
+
+                        JSONObject object1 = new JSONObject(result);
+                        String result1 = object.getString("country");
+
+
+                        final TextView txtInt = findViewById(R.id.txtInt);
+                        txtInt.setText(result1);
+
+
                     }
-                }catch (Exception e){
+                } catch (Exception e) {
                     e.printStackTrace();
                 }
-
+//        Http_End
 
             }
         }).start();
 
 
 //        Thread_Http_End
-
     }
+
 //        MENU_START
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -87,3 +120,4 @@ public class MainActivity extends AppCompatActivity {
     }
 //    MENU_END
 }
+
